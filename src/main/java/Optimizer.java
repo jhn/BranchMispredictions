4c97498ce9ccedcd18a95f0f2d7826552008a7ae
Johan Mena <jmena@pulsepoint.com>
2015-04-19 19:49:52 -0400

    Make CostModel a class

--- src/main/java/Optimizer.java
+++ src/main/java/Optimizer.java
@@ -35,29 +35,53 @@ public class Optimizer implements Callable<String> {
         Record R;
     }
 
+    private static class CostModel {
+
+        private double r;
+        private double t;
+        private double l;
+        private double m;
+        private double a;
+        private double f;
+
+        /**
+         *
+         * @param a the cost of writing an answer to the answer array and incrementing the answer array counter
+         * @param f the cost of applying function f to its argument
+         * @param l the cost of performing a logical “and”
+         * @param m the cost of a branch misprediction
+         * @param r the cost of accessing an array element rj[i] in order to perform operations on it
+         * @param t the cost of performing an if test
+         */
+        public CostModel(double a, double f, double l, double m, double r, double t) {
+            this.a = a;
+            this.f = f;
+            this.l = l;
+            this.m = m;
+            this.r = r;
+            this.t = t;
+        }
+    }
+
+    private final CostModel costModel;
     private final List<Double> selectivities;
-    private double r;
-    private double t;
-    private double l;
-    private double m;
-    private double a;
-    private double f;
 
     public Optimizer(List<Double> selectivities, Properties costs) {
         this.selectivities = selectivities;
-        this.r = Double.valueOf(costs.getProperty("r"));
-        this.t = Double.valueOf(costs.getProperty("t"));
-        this.l = Double.valueOf(costs.getProperty("l"));
-        this.m = Double.valueOf(costs.getProperty("m"));
-        this.a = Double.valueOf(costs.getProperty("a"));
-        this.f = Double.valueOf(costs.getProperty("f"));
+        double r = Double.valueOf(costs.getProperty("r"));
+        double t = Double.valueOf(costs.getProperty("t"));
+        double l = Double.valueOf(costs.getProperty("l"));
+        double m = Double.valueOf(costs.getProperty("m"));
+        double a = Double.valueOf(costs.getProperty("a"));
+        double f = Double.valueOf(costs.getProperty("f"));
+        this.costModel = new CostModel(a, f, l, m, r, t);
     }
 
     public String process() {
         // 1. Create an array A[] of size 2^k indexed by the subsets of S
-        int size = (int) Math.pow(2.0, selectivities.size()) - 1;
+        int size = (int) Math.pow(2.0, selectivities.size());
         Set<Record> records = new HashSet<>(size);
-        List<Set<Record>> sets = generatePowerset(records);
+        List<Set<Record>> sets = generatePowerset(records); // TODO: should be selectivities instead
         return "Process me! :-(";
     }
 
