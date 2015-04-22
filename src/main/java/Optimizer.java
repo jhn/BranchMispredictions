f9e89fa2e32dafeeb942077eea78c5856d7e2e11
Johan Mena <jmena@pulsepoint.com>
2015-04-22 19:54:55 -0400

    Refactoring and documentation overload.

--- src/main/java/Optimizer.java
+++ src/main/java/Optimizer.java
@@ -5,25 +5,33 @@ public class Optimizer implements Callable<String> {
 
     private static class SubSet {
 
-        /**
-         * values: The list of values for this subset.
-         * k: Number of terms corresponding to each subset.
-         * p: Product of the values of all terms in the subset.
-         * b: Whether the no-branch optimization was used to get the best cost.
-         * c: Current best cost for the subset.
-         * L: Left child of the sub plan.
-         * R: Right child of the sub plan.
-         */
+        Selectivities selectivities; // Container for the selectivities of this subset
+        int k;                       // Number of terms corresponding to each subset.
+        double p;                    // Product of the values of all terms in the subset.
+        boolean b;                   // Whether the no-branch optimization was used to get the best cost.
+        double c;                    // Current best cost for the subset.
+        SubSet L;                    // Left child of the sub plan.
+        SubSet R;                    // Right child of the sub plan.
 
-        Selectivities selectivities;
-        int k;
-        double p;
-        boolean b;
-        double c;
-        SubSet L;
-        SubSet R;
+        public static class CostModel {
+            double r; // the cost of accessing an array element rj[i] in order to perform operations on it
+            double t; // the cost of performing an if test
+            double l; // the cost of performing a logical “and”
+            double m; // the cost of a branch misprediction
+            double a; // the cost of writing an answer to the answer array and incrementing the answer array counter
+            double f; // the cost of applying function f to its argument
 
-        CostModel costModel;
+            public CostModel(double a, double f, double l, double m, double r, double t) {
+                this.a = a;
+                this.f = f;
+                this.l = l;
+                this.m = m;
+                this.r = r;
+                this.t = t;
+            }
+        }
+
+        public static CostModel costModel;
 
         public double noBranchCost() {
             return k * costModel.r + (k - 1) * costModel.l + costModel.f * k + costModel.a;
@@ -40,7 +48,7 @@ public class Optimizer implements Callable<String> {
 
         public static double combinedCost(SubSet one, SubSet two) {
             double q = Math.min(one.p, 1.0 - one.p);
-            return one.fixedCost() + one.costModel.m * q + one.p * two.c;
+            return one.fixedCost() + costModel.m * q + one.p * two.c;
         }
 
         public SubSet leftMost() {
@@ -64,65 +72,40 @@ public class Optimizer implements Callable<String> {
         }
     }
 
-    private static class CostModel {
-
-        double r;
-        double t;
-        double l;
-        double m;
-        double a;
-        double f;
-
-        /**
-         *
-         * @param a the cost of writing an answer to the answer array and incrementing the answer array counter
-         * @param f the cost of applying function f to its argument
-         * @param l the cost of performing a logical “and”
-         * @param m the cost of a branch misprediction
-         * @param r the cost of accessing an array element rj[i] in order to perform operations on it
-         * @param t the cost of performing an if test
-         */
-        public CostModel(double a, double f, double l, double m, double r, double t) {
-            this.a = a;
-            this.f = f;
-            this.l = l;
-            this.m = m;
-            this.r = r;
-            this.t = t;
-        }
-    }
-
-    private final CostModel costModel;
     private final List<Double> selectivities;
+    private List<SubSet> subSets;
 
-    public Optimizer(List<Double> selectivities, Properties costs) {
+    public Optimizer(List<Double> selectivities, Properties props) {
         this.selectivities = selectivities;
-        double r = Double.valueOf(costs.getProperty("r"));
-        double t = Double.valueOf(costs.getProperty("t"));
-        double l = Double.valueOf(costs.getProperty("l"));
-        double m = Double.valueOf(costs.getProperty("m"));
-        double a = Double.valueOf(costs.getProperty("a"));
-        double f = Double.valueOf(costs.getProperty("f"));
-        this.costModel = new CostModel(a, f, l, m, r, t);
+        this.subSets = generateSubSets(selectivities);
+        SubSet.costModel = parseCostModel(props);
+        initializeCosts(subSets);
     }
 
+    private SubSet.CostModel parseCostModel(Properties props) {
+        double r = Double.valueOf(props.getProperty("r"));
+        double t = Double.valueOf(props.getProperty("t"));
+        double l = Double.valueOf(props.getProperty("l"));
+        double m = Double.valueOf(props.getProperty("m"));
+        double a = Double.valueOf(props.getProperty("a"));
+        double f = Double.valueOf(props.getProperty("f"));
+        return new SubSet.CostModel(a, f, l, m, r, t);
+    }
+
+    /**
+     * Implements algorithm 4.11.
+     * @return C code that computes the optimal cost of a query given a selectivity.
+     * @throws Exception if things things go bananas.
+     */
     @Override
     public String call() throws Exception {
-        List<SubSet> subSets = generateSubSets(selectivities, costModel);
-
-        initializeCosts(subSets);
-
         for (SubSet s : subSets) {
             for (SubSet sPrime : subSets) {
                 if (!bitIntersect(s.selectivities.bitSet, sPrime.selectivities.bitSet)) {
-                    if (s.lemma48(sPrime)) {
-                        continue;
-                    } else if (sPrime.p <= 0.5 && s.lemma49(sPrime)) {
-                        continue;
-                    } else {
+                    if (!(s.lemma48(sPrime) || (sPrime.p <= 0.5 && s.lemma49(sPrime)))) {
                         double c = SubSet.combinedCost(sPrime, s);
                         List<Boolean> union = bitUnion(sPrime.selectivities.bitSet, s.selectivities.bitSet);
-                        SubSet subset = findSubSetByBits(subSets, union);
+                        SubSet subset = findSubSetByBitSet(subSets, union);
                         if (c < subset.c) {
                             subset.c = c;
                             subset.L = sPrime;
@@ -136,10 +119,17 @@ public class Optimizer implements Callable<String> {
         return "Process me! :-(";
     }
 
-    private static SubSet findSubSetByBits(List<SubSet> globalList, List<Boolean> bits) {
-        return globalList
+    /**
+     * Iterates over the global list of subsets and finds the one that matches the given bit set.
+     *
+     * @param allSubSets The global list of subsets.
+     * @param bitSet The bit set to look for.
+     * @return A subset that contains the specified bit set.
+     */
+    private static SubSet findSubSetByBitSet(List<SubSet> allSubSets, List<Boolean> bitSet) {
+        return allSubSets
                 .stream()
-                .filter(ss -> ss.selectivities.bitSet.equals(bits))
+                .filter(ss -> ss.selectivities.bitSet.equals(bitSet))
                 .findFirst().get();
     }
 
@@ -154,7 +144,7 @@ public class Optimizer implements Callable<String> {
         System.out.println();
     }
 
-    private static List<SubSet> generateSubSets(List<Double> selectivities, CostModel costModel) {
+    private static List<SubSet> generateSubSets(List<Double> selectivities) {
         int subSetSize = (int) (Math.pow(2.0, selectivities.size()) - 1);
 
         // gets a list initialized to (0, 1, 2... selectivities.size - 1)
@@ -164,7 +154,7 @@ public class Optimizer implements Callable<String> {
              numbers.add(i);
         }
         // generates the powerset for the list
-        List<List<Integer>> positions = powerSet(numbers);
+        List<List<Integer>> positions = orderedPowerSet(numbers);
         positions.remove(0); // get rid of the empty set
 
         // for each one of these lists of lists
@@ -179,24 +169,30 @@ public class Optimizer implements Callable<String> {
             subSet.k = Collections.frequency(bitSet, true);
             subSet.selectivities = new Selectivities(bitSet, selectivities);
             subSet.p = subSet.selectivities.calculateP();
-            subSet.costModel = costModel;
 
             subSets.add(subSet);
         }
         return subSets;
     }
 
+    /**
+     * Constructs a bit set with on bits set at the specified positions.
+     *
+     * @param positions The list of bits to initialize.
+     * @param length The total size of the bit set.
+     * @return A bit set with bits in 'positions' turned on.
+     */
     private static List<Boolean> bitSetFromPositions(List<Integer> positions, int length) {
-        Boolean bitSet[] = new Boolean[length];
-        for (int i = 0; i < length; i++) {
-            bitSet[i] = false;
-        }
-        for (int i = 0; i < positions.size(); i++) {
-            bitSet[positions.get(i)] = true;
-        }
-        return Arrays.asList(bitSet);
+        List<Boolean> bitSet = new ArrayList<>(Collections.nCopies(length, false));
+        positions.stream().forEach(i -> bitSet.set(i, true));
+        return bitSet;
     }
 
+    /**
+     * Initializes the logical and no-branching cost for each SubSet.
+     *
+     * @param subSets The SubSets to initialize;
+     */
     private static void initializeCosts(List<SubSet> subSets) {
         for (SubSet subSet : subSets) {
             double logicalAndCost = subSet.logicalAndCost();
@@ -210,12 +206,18 @@ public class Optimizer implements Callable<String> {
         }
     }
 
+    /**
+     * Creates a standard powerset from a given set.
+     *
+     * @param originalSet The starting set.
+     * @param <T> Type of the set.
+     * @return The powerset of the original set.
+     */
     public static <T> List<List<T>> powerSet(List<T> originalSet) {
-        List<List<T>> sets = new ArrayList<>();
         if (originalSet.isEmpty()) {
-            sets.add(new ArrayList<>());
-            return sets;
+            return Collections.singletonList(Collections.emptyList());
         }
+        List<List<T>> result = new ArrayList<>();
         List<T> list = new ArrayList<>(originalSet);
         T head = list.get(0);
         List<T> rest = new ArrayList<>(list.subList(1, list.size()));
@@ -223,11 +225,14 @@ public class Optimizer implements Callable<String> {
             List<T> newSet = new ArrayList<>();
             newSet.add(head);
             newSet.addAll(set);
-            sets.add(newSet);
-            sets.add(set);
+            result.add(newSet);
+            result.add(set);
         }
-        order(sets);
-        return sets;
+        return result;
+    }
+
+    public static <T> List<List<T>> orderedPowerSet(List<T> originalSet) {
+        return order(powerSet(originalSet));
     }
 
     public static class Selectivities {
@@ -245,16 +250,13 @@ public class Optimizer implements Callable<String> {
          * @return A set of selectivities that match the corresponding bit set.
          */
         public List<Double> getSelectivitiesFromBits() {
-            Double result[] = new Double[this.values.size()];
-            for (int i = 0; i < this.values.size(); i++) {
-                result[i] = 0.0;
-            }
-            for (int i = 0; i < this.bitSet.size(); i++) {
-                if (this.bitSet.get(i)) {
-                    result[i] = this.values.get(i);
+            List<Double> rs = new ArrayList<>(Collections.nCopies(values.size(), 0.0));
+            for (int i = 0; i < bitSet.size(); i++) {
+                if (bitSet.get(i)) {
+                    rs.set(i, values.get(i));
                 }
             }
-            return Arrays.asList(result);
+            return rs;
         }
 
         /**
