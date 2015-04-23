ef415d49bbade6aaa2d5897c7e44fce6487ef9ed
Johan Mena <jmena@pulsepoint.com>
2015-04-22 21:49:28 -0400

    Update run script

--- stage2.sh
+++ stage2.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-java -cp src/main/java Main $1 $2
+JAVA_VER=$(java -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+JAVA=java
+
+if [ "$JAVA_VER" -ge 18 ]; then
+   JAVA=java
+else
+   JAVA=/local/jdk1.8.0_31/bin/java
+fi
+
+$JAVA -cp src/main/java Main $1 $2
